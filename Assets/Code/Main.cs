bf1d99149aa2ae0e14ae08b0c2fc7999d3064611
zaniocz <zaniocz@gmail.com>
2022-03-23 15:52:13 -0500

    Time in UI Text Working

--- Assets/Code/Main.cs
+++ Assets/Code/Main.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 //pendiente revisar esta guia https://forum.unity.com/threads/converting-seconds-to-hours-minutes-seconds-for-count-down-timer.861388/
 
@@ -8,12 +10,13 @@ public class Main : MonoBehaviour
 {
 
     bool play = false;
-    float currentTime = 8598;
+    float currentTime = 0;
     float liveTime = 0;
     int minutes = 0;
     int seconds = 0;
     int hour = 0;
 
+    public TMP_Text text;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,23 +28,18 @@ public class Main : MonoBehaviour
     {
         if (play)
         {
-            //minutes = Mathf.FloorToInt((Time.time - currentTime)/60f);
-            //seconds = Mathf.FloorToInt((Time.time - currentTime) - minutes * 60f);
-            //hour = Mathf.FloorToInt((Time.time - currentTime)/ 3600f);
-
+            //text.text = string.Format("{0:0}:{1:00}:{2:00}", TimeSpan.FromSeconds(currentTime + Time.time).Hours, TimeSpan.FromSeconds(currentTime + Time.time).Minutes, TimeSpan.FromSeconds(currentTime + Time.time).Seconds);
+            text.text = string.Format("{0:0}:{1:00}:{2:00}", TimeSpan.FromSeconds(Time.time - currentTime).Hours, TimeSpan.FromSeconds(Time.time - currentTime).Minutes, TimeSpan.FromSeconds(Time.time - currentTime).Seconds);
+            //Debug.Log(string.Format("{0:0}:{1:00}:{2:00}", TimeSpan.FromSeconds(currentTime + Time.time).Hours, TimeSpan.FromSeconds(currentTime + Time.time).Minutes, TimeSpan.FromSeconds(currentTime + Time.time).Seconds));
             seconds = Mathf.FloorToInt((currentTime + Time.time) % 60f);
             minutes = Mathf.FloorToInt((currentTime+ Time.time) / 60f);
             hour = Mathf.FloorToInt((currentTime+ Time.time) / 3600f);
-
-
-            Debug.Log(string.Format("{0:0}:{1:00}:{2:00}", hour, minutes, seconds));
-            //Debug.Log(Time.time- currentTime);
         }
         
     }
 
     public void playTime() {
-       // currentTime = Time.time;
+        currentTime = Time.time;
         if (play)
         {
             play = false;
